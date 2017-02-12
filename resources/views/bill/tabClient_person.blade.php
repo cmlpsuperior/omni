e37a1c6ed549272004e69d15bb3b6173c74e6675
cmlpsuperior <henryespinozat@gmail.com>
2017-02-11 19:03:57 -0500

    avanzando

--- resources/views/bill/tabClient_person.blade.php
+++ resources/views/bill/tabClient_person.blade.php
@@ -1,15 +1,35 @@
-<ul class="nav nav-tabs">
-  <li role="presentation" ><a href="#noClient" aria-controls="noClient" role="tab" data-toggle="tab">No cliente</a></li>
-  <li role="presentation" ><a href="#client" aria-controls="client" role="tab" data-toggle="tab">Cliente</a></li>
-</ul>
-
 <div class="tab-content">
-  <div role="tabpanel" class="tab-pane active" id="noClient">
+  <div role="tabpanel" class="tab-pane active" id="client">
     <div class="row">
-      
+      <div class="col-xs-6">
+      	<div class="form-group">
+	      	<label for="documentNumber">DNI *</label>
+	      	<input class="form-control" id="documentNumber" name="documentNumber" type="text">
+	     </div>
+      </div>
+
+      <div class="col-xs-6">
+      	<div class="form-group">
+	      	<label for="shippingAddress">RUC *</label>
+	      	<input class="form-control" id="shippingAddress" name="shippingAddress" type="text" value="{{ old('shippingAddress') }}">
+	     </div>
+      </div>
     </div>
-  </div>
-  <div role="tabpanel" class="tab-pane" id="client">
-    
+
+    <div class="row">
+      <div class="col-xs-6">
+      	<div class="form-group">
+	      	<label for="nameRead">Nombre cliente</label>
+	      	<input class="form-control" id="nameRead" name="nameRead" type="text">
+	     </div>
+      </div>
+
+      <div class="col-xs-6">
+      	<div class="form-group">
+	      	<label for="shippingAddress"></label>
+	      	<input class="form-control" id="shippingAddress" name="shippingAddress" type="text" value="{{ old('shippingAddress') }}">
+	     </div>
+      </div>
+    </div>
   </div>
 </div>
