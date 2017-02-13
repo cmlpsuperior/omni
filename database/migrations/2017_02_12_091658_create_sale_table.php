a6813e188044101fbcc901b72034de68f0722897
cmlpsuperior <henryespinozat@gmail.com>
2017-02-13 11:47:04 -0500

    sale and saleItem tables

---
+++ database/migrations/2017_02_12_091658_create_sale_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSaleTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('sale', function (Blueprint $table) {
+            $table->increments('idSale');
+
+            $table->datetime('registerDate');
+            $table->datetime('discount');
+            $table->double('totalAmount', 15, 2);
+
+            $table->string('state', 50);
+            $table->boolean('isSale');
+            $table->string('observations', 250)->nullable();
+            
+            $table->integer('idClient')->unsigned()->nullable();
+            $table->integer('idZone')->unsigned();
+            $table->integer('idEmployee')->unsigned();
+
+            $table->foreign('idClient')->references('idClient')->on('client');
+            $table->foreign('idZone')->references('idZone')->on('zone');
+            $table->foreign('idEmployee')->references('idEmployee')->on('employee');
+        });
+    }
+
+    
+    public function down()
+    {
+        Schema::drop('sale');
+    }
